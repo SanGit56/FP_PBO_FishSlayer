55451ff6251d3ecf077aa32d26e86471b4d3b310
Farah Dhia Fadhila <91003215+farah-dhiaf@users.noreply.github.com>
2022-12-16 15:07:43 +0700

    Update FishSlayer.java

--- src/application/FishSlayer.java
+++ src/application/FishSlayer.java
@@ -49,7 +49,19 @@ public class FishSlayer extends Application{
 	List<Ocean> oceans;
 	List<Fish> fishes;
 	
+	private double mouseX;
+	private int score;
+	
 	@Override public void start(Stage arg0) throws Exception {
+	}
+	
+	private void setup() {
+		oceans = new ArrayList<>();
+		nets = new ArrayList<>();
+		fishes = new ArrayList<>();
+		player = new Ship(WIDTH / 2, HEIGHT - PLAYER_SIZE, PLAYER_SIZE, PLAYER_IMG);
+		score = 0;
+		IntStream.range(0, MAX_FISHES).mapToObj(i -> this.newFish()).forEach(fishes::add);
 	}
 	
 //	player
@@ -94,5 +106,101 @@ public class FishSlayer extends Application{
 			exploding = true;
 			explosionStep = -1;
 		}
+	}
+	
+	public class Fish extends Ship {
+		int SPEED = (score / 5) + 2;
+		
+		public Fish (int posX, int posY, int size, Image image) {
+			super(posX, posY, size, image);
+		}
+		
+		public void update() {
+			super.update();
+			if(!exploding && !destroyed) posY += SPEED;
+			if(posY > HEIGHT) destroyed = true;
+		}
+	}
+	
+	public class Shot {
+		public boolean toRemove;
+		
+		int posX = 10;
+		int posY = 10;
+		int speed = 30;
+		static final int size = 6;
+		
+		public Shot (int posX, int posY) {
+			this.posX = posX;
+			this.posY = posY;
+		}
+		
+		public void update() {
+			posY -= speed;
+		}
+		
+		public void draw() {
+			gc.setFill(Color.RED);
+			if(score >= 20 && score <= 40 || score >= 120){
+				gc.setFill(Color.YELLOWGREEN);
+				speed = 50;
+				gc.fillRect(posX - 5, posY - 10, size + 10, size + 30);
+			} 
+			else {
+				gc.fillOval(posX, posY, size, size);
+			}
+		}
+		
+		public boolean collide(Ship other) {
+			int distance = distance(this.posX + size / 2, this.posY + size / 2,
+					       other.posX + other.size / 2, other.posY + other.size / 2);
+			return distance < other.size / 2 + size / 2;
+		}
+	}
+	
+	public class Ocean {
+		int posX;
+		int posY;
+		private int h;
+		private int w;
+		private int r;
+		private int g;
+		private int b;
+		
+		public Universe() {
+			posX = RAND.nextInt(WIDTH);
+			posY = 0;
+			
+			w = RAND.nestInt(5) + 1;
+			h = RAND.nestInt(5) + 1;
+			r = RAND.nestInt(100) + 150;
+			g = RAND.nestInt(100) + 150;
+			b = RAND.nestInt(100) + 150;
+			
+			opacity = RAND.nextFloat();
+			if(opacity < 0) opacity *= -1;
+			if(opacity > 0.5) opacity = 0.5;
+		}
+		
+		public void draw() {
+			if(opacity > 0.8) opacity -= 0.01;
+			if(opacity < 0.1) opacity += 0.01;
+			gc.setFill(Color.rgb(r, g, b, opacity));
+			gc.fillOval(posX, posY, w, h);
+			posY += 20;
+		}
+	}
+	
+	Fish newFish() {
+		return new Fish(50 + RAND.nextInt(WIDTH - 100), 0, PLAYER_SIZE, FISHES_IMG[RAND.nextInt(FISHES_IMG.length)]);
+		
+	}
+	
+	int distance (int x1, int y1, int x2, int y2) {
+		return (int) Math.sqrt(Math.pow(x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	}
+	
+	public static void main(String[] args) {
+		launch();
 	}
 }
